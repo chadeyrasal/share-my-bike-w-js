627ccceb2c91dcba7653a2c2cc6eb27c74c4279c
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-05-08 18:16:38 +0100

    add has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  has_secure_password
+
   has_many :bicycles, foreign_key: :owner_id
   has_many :trips, through: :bicycles
   has_many :reservations, foreign_key: :renter_id, class_name: "Trip"
