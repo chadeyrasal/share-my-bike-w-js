ab439d3884efbaeb2745d6c57aa5e7163bd3b08a
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-05-08 14:22:23 +0100

    add new routes for signup and login and create three new controllers for auth: owners, renters and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,18 @@ Rails.application.routes.draw do
 
   root "welcome#home"
 
+  get "/renters/new", to: "renters#new", as: :signup
+
+  get "/owners/new", to: "owners#new", as: :owner_signup
+
+  get "/login", to: "sessions#new"
+
+  post "/login", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy"
+
+  resources :renters, only: [:create]
+
+  resources :owners, only: [:create]
+
 end
