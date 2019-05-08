a5def51eb02d61eb9308f72e9e3eda5213c0202e
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-05-08 16:40:38 +0100

    add neighborhood id, title and price attributed to bicycles

--- db/migrate/20190507180951_create_bicycles.rb
+++ db/migrate/20190507180951_create_bicycles.rb
@@ -1,12 +1,13 @@
 class CreateBicycles < ActiveRecord::Migration[5.2]
   def change
     create_table :bicycles do |t|
-      t.string :category
+      t.string :bicycle_type
       t.string :size
       t.string :colour
+      t.string :title
       t.string :description
-      t.string :country
-      t.string :city
+      t.string :price
+      t.integer :neighborhood_id
       t.integer :owner_id
 
       t.timestamps
