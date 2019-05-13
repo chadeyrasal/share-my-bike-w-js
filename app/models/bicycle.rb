b792944256a2bccbc84ab6add0808b75d5942cef
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-05-13 20:25:38 +0100

    add validation to bicycle model

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -5,6 +5,8 @@ class Bicycle < ApplicationRecord
   has_many :trips
   has_many :renters, class_name: "User", through: :trips
 
+  validates :title, :description, presence: true
+
   SIZE = ["Small", "Medium", "Large"]
 
 end
