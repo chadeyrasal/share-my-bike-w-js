571623fe4fdba4a2fc10ace4445da15a5b2a044c
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-05-16 21:04:34 +0100

    add index action to neighborhoods controller

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,5 +1,9 @@
 class NeighborhoodsController < ApplicationController
 
+  def index
+    @neighborhoods = Neighborhood.all
+  end
+
   def show
     @neighborhood = Neighborhood.find_by(id: params[:id])
   end
