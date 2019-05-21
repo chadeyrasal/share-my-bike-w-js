a03e70187914d4a8eac1ac5ebbe9bcd91ade3ac2
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-05-21 18:59:53 +0100

    redirect user to citis index after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to bicycles_path
+      redirect_to cities_path
     else
       render :new
     end
