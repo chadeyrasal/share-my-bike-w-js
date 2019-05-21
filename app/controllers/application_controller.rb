1869338104843e7702a206ff9347d30d5484500d
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-05-21 18:50:12 +0100

    modify layout view to start a nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   helper_method :current_user
+  helper_method :logged_in?
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
