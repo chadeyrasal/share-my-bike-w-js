23c2b670d688f2d7269d052f030bc3fee457ae01
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-05-23 18:13:02 +0100

    modify create action for sessions controller. basic login is now fully functional

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,11 @@
 class SessionsController < ApplicationController
 
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.find_by(email: params[:user][:email])
-    if @user && @user.authenticate(params[:user][:password])
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to root_path
     else
