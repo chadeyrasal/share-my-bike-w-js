cc29abe6f2a5bf1df4b085d12b52ff6d15fdc43b
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-05-23 17:37:25 +0100

    modify post sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#new"
 
-  post "/login", to: "sessions#create"
+  post "/sessions", to: "sessions#create"
 
   get "/logout", to: "sessions#destroy"
 
