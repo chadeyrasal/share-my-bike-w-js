a01ffe4294c65166e1b00842c503aec7416488a8
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-05-24 18:15:23 +0100

    add oauth part to sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,29 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to root_path
-    else
-      redirect_to login_path
+    if auth_hash = request.env["omniauth.auth"] # auth via omniauth
+      oauth_email = auth_hash["info"]["email"]
+      oauth_name = auth_hash["info"]["name"]
+      if @user = User.find_by(email: oauth_email)
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        @user = User.new(full_name: oauth_name, email: oauth_email, password: SecureRandom.hex)
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to root_path
+        else
+          redirect_to login_path
+        end
+      end
+    else # auth via classic login
+      @user = User.find_by(email: params[:email])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        redirect_to login_path
+      end
     end
   end
 
