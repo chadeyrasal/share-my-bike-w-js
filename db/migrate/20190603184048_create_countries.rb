081b9a65bf6ad409f26b743136a23f578554d399
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-06-03 19:52:22 +0100

    add timestamps to countries table

--- db/migrate/20190603184048_create_countries.rb
+++ db/migrate/20190603184048_create_countries.rb
@@ -2,6 +2,8 @@ class CreateCountries < ActiveRecord::Migration[5.2]
   def change
     create_table :countries do |t|
       t.string :name
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2019_06_03_184048) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "neighborhoods", force: :cascade do |t|
