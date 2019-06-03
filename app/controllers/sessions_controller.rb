bf64226aa31430b44623dd4888a787247317710b
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-06-03 17:00:42 +0100

    refactor sessions controller after omniauth implemented to avoid fat controller. Create class method in user model for that

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,20 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     if auth_hash = request.env["omniauth.auth"] # auth via omniauth
-      oauth_email = auth_hash["info"]["email"]
-      oauth_name = auth_hash["info"]["name"]
-      if @user = User.find_by(email: oauth_email)
-        session[:user_id] = @user.id
-        redirect_to root_path
-      else
-        @user = User.new(full_name: oauth_name, email: oauth_email, password: SecureRandom.hex)
-        if @user.save
-          session[:user_id] = @user.id
-          redirect_to root_path
-        else
-          redirect_to login_path
-        end
-      end
+      @user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id] = @user.id
+      redirect_to root_path
     else # auth via classic login
       @user = User.find_by(email: params[:email])
       if @user && @user.authenticate(params[:password])
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,25 @@ class User < ApplicationRecord
   has_many :trips, through: :bicycles
   has_many :reservations, foreign_key: :renter_id, class_name: "Trip"
 
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    where(email: auth_hash["info"]["email"], full_name: auth_hash["info"]["name"]).first_or_create do |user|
+      user.password = SecureRandom.hex
+    end
+#    oauth_email =
+#    oauth_name =
+#    if @user = User.find_by(email: oauth_email)
+#      session[:user_id] = @user.id
+#      redirect_to root_path
+#    else
+#      @user = User.new(full_name: oauth_name, email: oauth_email, password: SecureRandom.hex)
+#      if @user.save
+#        session[:user_id] = @user.id
+#        redirect_to root_path
+#      else
+#        redirect_to login_path
+#      end
+#    end
+  end
+
 end
