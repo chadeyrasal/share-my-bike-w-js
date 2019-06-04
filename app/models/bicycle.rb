39d57ceb9919269daa2a371f89207f3066578000
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-06-04 17:05:19 +0100

    add extra AR relationships for bicycles and country

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,6 +1,7 @@
 class Bicycle < ApplicationRecord
 
   belongs_to :neighborhood
+  belongs_to :country
   belongs_to :owner, class_name: "User"
   has_many :trips
   has_many :renters, class_name: "User", through: :trips
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,7 @@
 class Country < ApplicationRecord
 
   has_many :cities
+  has_many :bicycles
 
 
   def self.alphabetically
