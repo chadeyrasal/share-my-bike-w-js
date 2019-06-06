7cebe27ffed89759a64855e3d79a0b3381dcc24d
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-06-06 12:54:10 +0100

    remove rating constant in trip model and the corresponding variable in the controller. other solution found

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -35,7 +35,6 @@ class TripsController < ApplicationController
     @trip = Trip.find_by(id: params[:id])
     @user = @trip.renter
     @bicycle = @trip.bicycle
-    @rating_opt = Trip::RATING
   end
 
   def update
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,7 +3,4 @@ class Trip < ApplicationRecord
   belongs_to :bicycle
   belongs_to :renter, class_name: "User"
 
-
-  RATING = [1, 2, 3, 4, 5]
-
 end
