f8ccc8f10daf62b6f7df1fe94b1a8496f791689d
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-06-07 10:32:26 +0100

    add redirect_unless_logged_in method to the list of helpers ot be used in the whole app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
   helper_method :logged_in?
+  helper_method :redirect_unless_logged_in
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
