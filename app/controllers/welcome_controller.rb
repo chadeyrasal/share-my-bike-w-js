f7801be64a9e9e654308bda873d6a377652dd148
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-06-13 13:45:04 +0100

    write scope method that allows display of 5 most popular bicycles to homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
 
   def home
     @latest = Bicycle.latest
+    @most_popu = Bicycle.joins(:trips).group(:bicycle_id).count.sort_by{|_key, value| value}.reverse
   end
 
 end
