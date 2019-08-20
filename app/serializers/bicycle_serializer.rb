cdfebe59e6fdbb68e498fe332976c395a3ae9776
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-08-20 19:08:07 +0100

    create bicycleserializer and add attributes and relationships

---
+++ app/serializers/bicycle_serializer.rb
@@ -0,0 +1,10 @@
+class BicycleSerializer < ActiveModel::Serializer
+
+  attributes :id, :bicycle_type, :size, :colour, :title, :description, :price
+
+  belongs_to :neighborhood
+  belongs_to :city
+  belongs_to :country
+  belongs_to :owner, class_name: "User"
+
+end
