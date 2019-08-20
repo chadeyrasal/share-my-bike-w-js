47037a4d117828437661112086ae316bdec08bc5
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-08-20 20:29:02 +0100

    create bicycle constructor and 1 prototype method

--- app/assets/javascripts/bicycles.js
+++ app/assets/javascripts/bicycles.js
@@ -5,6 +5,37 @@ $(() => {
 const bindClickHandlers = () => {
   $('.city_bicycles').on('click', (event) => {
     event.preventDefault();
-    console.log(fetch(`/bicycles.json`))
+    alert('hello there')
+//    fetch(`/bicycles.json`)
+//      .then(response => response.json())
+//      .then(bicycles => {
+//        $('#app-container').html('');
+//        bicycles.forEach((bicycle) => {
+//          let newBicycle = new Bicycle(bicyle);
+//          let bicycleHtml = newBicycle.formatIndex();
+//          $('#app-container').append(bicycleHtml);
+//        })
+//      })
   })
+}
+
+function Bicycle(id, bicycle_type, size, colour, title, desription, price, neighborhood, owner, country, city) {
+  this.id = id;
+  this.bicycle_type = bicycle_type;
+  this.size = size;
+  this.colour = colour;
+  this.title = title;
+  this.description = description;
+  this.price = price;
+  this.neighborhood = neighborhood;
+  this.owner = owner;
+  this.country = country;
+  this.city = city;
+}
+
+Bicycle.prototype.formatIndex = function() {
+  let bicycleHtml = `
+    <h1>${this.title}</h1>
+  `;
+  return bicycleHtml;
 }
