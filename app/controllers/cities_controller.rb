ce8e9836a5eb6bf81239908ac720ba2ad9e7e912
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-08-21 13:36:13 +0100

    add json formatting to index and show actions

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,10 +3,18 @@ class CitiesController < ApplicationController
   def index
     @cities = City.all
     @countries = Country.alphabetically
+    respond_to do |f|
+      f.html
+      f.json { render json: @cities }
+    end
   end
 
   def show
     @city = City.find_by(id: params[:id])
+    respond_to do |f|
+      f.html
+      f.json { render json: @city }
+    end
   end
 
 end
