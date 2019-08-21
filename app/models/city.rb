8c7cfce480a32b9f10601824e8a1e922e896272e
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-08-21 18:41:53 +0100

    add next and previous methods

--- app/models/city.rb
+++ app/models/city.rb
@@ -5,4 +5,12 @@ class City < ApplicationRecord
   has_many :bicycles
   has_many :bicycles, through: :neighborhoods
 
+  def next
+    City.where("id > ?", id).first || City.first
+  end
+
+  def previous
+    City.where("id < ?", id).last || City.last
+  end
+
 end
