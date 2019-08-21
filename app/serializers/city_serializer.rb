25413605f94c10739795e23ebd49de2e7182fffa
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-08-21 18:19:58 +0100

    add relationships to city serializer

--- app/serializers/city_serializer.rb
+++ app/serializers/city_serializer.rb
@@ -2,4 +2,7 @@ class CitySerializer < ActiveModel::Serializer
   attributes :id, :name
 
   belongs_to :country
+  has_many :neighborhoods
+  has_many :bicycles
+  has_many :bicycles, through: :neighborhoods
 end
