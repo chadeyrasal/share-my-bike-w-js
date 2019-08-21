f87dd86b528fe11f38bbc433dc4cd592a07a16d7
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-08-21 13:29:11 +0100

    comment content of bicycles.js

--- app/assets/javascripts/bicycles.js
+++ app/assets/javascripts/bicycles.js
@@ -1,12 +1,12 @@
-$(() => {
-  bindClickHandlers();
-})
+//$(() => {
+//  bindClickHandlers();
+//})
 
-const bindClickHandlers = () => {
-  $('.city_bicycles').on('click', (event) => {
-    event.preventDefault();
-    history.pushState(null, null, 'bicycles');
-    alert('hello there')
+//const bindClickHandlers = () => {
+//  $('.city_bicycles').on('click', (event) => {
+//    event.preventDefault();
+//    history.pushState(null, null, 'bicycles');
+//    alert('hello there')
 //    fetch(`/bicycles.json`)
 //      .then(response => response.json())
 //      .then(bicycles => {
@@ -17,26 +17,26 @@ const bindClickHandlers = () => {
 //          $('#app-container').append(bicycleHtml);
 //        })
 //      })
-  })
-}
+//  })
+//}
 
-function Bicycle(id, bicycle_type, size, colour, title, desription, price, neighborhood, owner, country, city) {
-  this.id = id;
-  this.bicycle_type = bicycle_type;
-  this.size = size;
-  this.colour = colour;
-  this.title = title;
-  this.description = description;
-  this.price = price;
-  this.neighborhood = neighborhood;
-  this.owner = owner;
-  this.country = country;
-  this.city = city;
-}
+//function Bicycle(id, bicycle_type, size, colour, title, desription, price, neighborhood, owner, country, city) {
+//  this.id = id;
+//  this.bicycle_type = bicycle_type;
+//  this.size = size;
+//  this.colour = colour;
+//  this.title = title;
+//  this.description = description;
+//  this.price = price;
+//  this.neighborhood = neighborhood;
+//  this.owner = owner;
+//  this.country = country;
+//  this.city = city;
+//}
 
-Bicycle.prototype.formatIndex = function() {
-  let bicycleHtml = `
-    <a href="/bicycles/${this.id}"><h1>${this.title}</h1></a>
-  `;
-  return bicycleHtml;
-}
+//Bicycle.prototype.formatIndex = function() {
+//  let bicycleHtml = `
+//    <a href="/bicycles/${this.id}"><h1>${this.title}</h1></a>
+//  `;
+//  return bicycleHtml;
+//}
