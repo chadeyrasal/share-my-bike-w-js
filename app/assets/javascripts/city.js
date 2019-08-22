4c86a60e9337af8f4de6f05dd97e06bd9de5b380
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-08-22 12:45:33 +0100

    refactor index and show js

--- app/assets/javascripts/city.js
+++ app/assets/javascripts/city.js
@@ -1,50 +1,66 @@
-$(() => {
-  cityClickHandlers();
-})
+const baseUrl = 'http://localhost:3000/';
 
-const cityClickHandlers = () => {
+$(() => {
+  allCitiesClick();
+  cityClick();
+});
+
+const clearDom = () => {
+  $('#app-container').html('');
+};
+
+const allCitiesClick = () => {
   $('.all_cities').on('click', (event) => {
     event.preventDefault();
-//    history.pushState(null, null, 'cities');
-//    fetch('/cities.json')
-//      .then(response => response.json())
-//      .then(cities => {
-//        $('#app-container').html('');
-//        cities.forEach(city => {
-//          let newCity = new City(city);
-//          let cityHtml = newCity.formatIndex();
-//          $('#app-container').append(cityHtml);
-//        })
-//      })
-  })
+    let url = baseUrl + 'cities'
+    history.pushState(null, null, url);
+    getCities();
+  });
+};
 
+const getCities = () => {
+  fetch('/cities.json')
+    .then(response => response.json())
+    .then(cities => {
+      clearDom();
+      cities.forEach(city => {
+        let newCity = new City(city);
+        let cityHtml = newCity.formatIndex();
+        $('#app-container').append(cityHtml);
+      });
+    });
+};
+
+const cityClick = () => {
   $(document).on('click', '.show-city-link', function(event) {
     event.preventDefault();
-    let id = $(this).attr('data-id')
-    history.pushState(null, null, `cities/${id}`);
+    clearDom();
+    let id = $(this).attr('data-id');
+    let url = baseUrl + 'cities/' + id;
+    history.pushState(null, null, url);
     fetch(`/cities/${id}.json`)
       .then(response => response.json())
       .then(city => {
-        $('#app-container').html('');
         let newCity = new City(city);
         let cityHtml = newCity.formatShow();
         $('#app-container').append(cityHtml);
-      })
-  })
+      });
+  });
+};
 
-  $(document).on('click', '.js-next-city', (event) => {
-    event.preventDefault()
-    let id = $(this).attr('data-id')
-    console.log(id)
+
+//  $(document).on('click', '.js-next-city', (event) => {
+//    event.preventDefault()
+//    let id = $(this).attr('data-id')
+//    console.log(id)
 //    fetch(`cities/${id}/next`)
-  })
+//  })
 
-  $(document).on('click', '.js-previous-city', (event) => {
-    event.preventDefault()
-    let id = $(this).attr('data-id')
-    console.log(id)
-  })
-}
+//  $(document).on('click', '.js-previous-city', (event) => {
+//    event.preventDefault()
+//    let id = $(this).attr('data-id')
+//    console.log(id)
+//  })
 
 function City(object) {
   this.id = object.id;
