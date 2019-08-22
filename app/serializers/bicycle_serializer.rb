84fa6445b9bfc5df53d606e226fa3e6ff3a9ec9f
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-08-22 08:16:34 +0100

    add attributes to bicycle serializer

--- app/serializers/bicycle_serializer.rb
+++ app/serializers/bicycle_serializer.rb
@@ -1,6 +1,6 @@
 class BicycleSerializer < ActiveModel::Serializer
 
-  attributes :id, :bicycle_type, :size, :colour, :title, :description, :price
+  attributes :id, :bicycle_type, :size, :colour, :title, :description, :price, :neighborhood, :city, :country, :owner
 
   belongs_to :neighborhood
   belongs_to :city
